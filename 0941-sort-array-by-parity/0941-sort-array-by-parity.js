9ede739c1f201ab833bf7606cb7b9bf5deaa4b1d
Tafsir Ahamed <63195907+Tafsir1729@users.noreply.github.com>
2023-09-28 18:24:33 +0600

    Time: 65 ms (62.51%), Space: 44.8 MB (37.81%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function (nums) {
+    let result = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 === 0) {
+            result.push(nums[i])
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 != 0) {
+            result.push(nums[i])
+        }
+    }
+    return result;
+};
