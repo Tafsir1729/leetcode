177dc6ba70fe506f61c5a703c1a12a9daa667327
Tafsir Ahamed <63195907+Tafsir1729@users.noreply.github.com>
2023-10-02 13:00:23 +0600

    Time: 54 ms (41.46%), Space: 42.1 MB (31.67%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,5 +4,15 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+    for (let i = 0; i < haystack.length; i++) {
+    let isMatched = true;
+    for (let j = 0; j < needle.length; j++) {
+      if (haystack[i + j] !== needle[j]) {
+        isMatched = false;
+        break;
+      }
+    }
+    if (isMatched) return i;
+  }
+  return -1;
 };
